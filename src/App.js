ed01b19d4346b7cb470a114fd1e339687c8a5d30
Balu Ravi <balu89.rs@gmail.com>
2023-09-05 12:46:01 +0530

    prop driling basics

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css';
-import { Home } from './pages/Home';
-import { Menu } from './pages/Menu';
-import { Contact } from './pages/Contact';
-import { Navbar } from './pages/Navbar.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./pages/Navbar.js";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("Balu");
+
   return (
     <div className="App">
-
       <Router>
-
         <Navbar />
 
-
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/menu' element={<Menu />}></Route>
-          <Route path='/contact' element={<Contact />} />
-          <Route path='*' element={<h1>404 Page Not Found</h1>} />
+          <Route path="/" element={<Home username={username} />} />
+          <Route
+            path="/profile"
+            element={<Profile username={username} setUsername={setUsername} />}
+          ></Route>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1>404 Page Not Found</h1>} />
         </Routes>
-
       </Router>
-
     </div>
   );
 }
